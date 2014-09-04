fb517a0c4b3a192ffa73a5cb990021587e8f1343
lightspeedcrypto <lightspeedcrypto@gmail.com>
2014-09-04 11:30:28 +0100

    first commit

---
+++ src/qt/qtipcserver.h
@@ -0,0 +1,10 @@
+#ifndef QTIPCSERVER_H
+#define QTIPCSERVER_H
+
+// Define lightspeed-Qt message queue name
+#define lightspeedURI_QUEUE_NAME "lightspeedURI"
+
+void ipcScanRelay(int argc, char *argv[]);
+void ipcInit(int argc, char *argv[]);
+
+#endif // QTIPCSERVER_H
